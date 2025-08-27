1efbbefa7b0cf30c20720be33c6888a6ec7ed0f5
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-08-27 16:31:57 +0300

    5. Build the SettingsScreen UI (TextField & DropdownButton)

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -8,11 +8,37 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  final _nameController = TextEditingController();
+  final _images = ['Lake', 'Mountain', 'Sea', 'Country'];
+  final _selectedImage = 'Lake';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Settings')),
-      body: const Center(child: Text('Settings Screen')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                hintText: 'Enter your name',
+                labelText: 'Name',
+              ),
+            ),
+            DropdownButton(
+              value: _selectedImage,
+              items: _images
+                  .map(
+                    (value) =>
+                        DropdownMenuItem(value: value, child: Text(value)),
+                  )
+                  .toList(),
+              onChanged: (newValue) {},
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
