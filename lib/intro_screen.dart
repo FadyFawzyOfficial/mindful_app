757034adae2b3edbacd972a4492777ee5d245068
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-08-27 14:22:59 +0300

    3. Use Scaffold, Text & Add Stack and Image

--- lib/intro_screen.dart
+++ lib/intro_screen.dart
@@ -5,6 +5,16 @@ class IntroScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Welcome Screen')),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset('assets/images/sea.jpg', fit: BoxFit.cover),
+          ),
+          Center(child: Text('Welcome', style: TextStyle(fontSize: 24))),
+        ],
+      ),
+    );
   }
 }
