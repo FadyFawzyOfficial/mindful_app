a89de64737e1eb87eeec8bab33ca92368f8982c3
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2025-08-27 16:45:28 +0300

    6. Connect the UI to the Business Logic

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'data/sp_helper.dart';
+
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
@@ -10,7 +12,14 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   final _nameController = TextEditingController();
   final _images = ['Lake', 'Mountain', 'Sea', 'Country'];
-  final _selectedImage = 'Lake';
+  var _selectedImage = 'Lake';
+
+  @override
+  void initState() {
+    super.initState();
+    getSettings();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,11 +43,33 @@ class _SettingsScreenState extends State<SettingsScreen> {
                         DropdownMenuItem(value: value, child: Text(value)),
                   )
                   .toList(),
-              onChanged: (newValue) {},
+              onChanged: (newValue) =>
+                  setState(() => _selectedImage = newValue ?? 'Lake'),
             ),
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: saveSettings,
+        child: const Icon(Icons.save_rounded),
+      ),
     );
+  }
+
+  Future saveSettings() async {
+    final spHelper = SPHelper();
+    await spHelper.setSettings(
+      name: _nameController.text,
+      image: _selectedImage,
+    );
+  }
+
+  Future<void> getSettings() async {
+    final spHelper = SPHelper();
+    final settings = await spHelper.getSettings();
+    setState(() {
+      _nameController.text = settings[SPHelper.nameKey] ?? '';
+      _selectedImage = settings[SPHelper.imageKey] ?? 'Lake';
+    });
   }
 }
